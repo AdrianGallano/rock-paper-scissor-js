c112ff2eb706cd75957528131ec32e4fc29d29db
adrian <adriangallanoengr@gmail.com>
2022-02-12 07:54:14 +0800

    Redo layout and style

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const playerChoose = () => {
+	let choices = document.getElementsByClassName("game-choice") 
+	Array.from(choices).forEach(choice=>choice.addEventListener("click", (e)=>{
+		console.log(choice.getAttribute("key-choice"))
+	}))
+}
 
+const computerChoose = () => {
+	let choices = ["rock", "paper", "scissor"]
+	let randNum = Math.floor(Math.random() * 3)
+	return choices[randNum]
+}
 
+const round = (playerChoice, computerChoice) => {
+
+}
+
+const sayHello = () => {
+	console.log("hello")
+}
 
