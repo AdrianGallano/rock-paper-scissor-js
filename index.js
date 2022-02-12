aa256a054157b211d7939d34b11d224247437249
adrian <adriangallanoengr@gmail.com>
2022-02-12 11:20:56 +0800

    Mechanics done

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
-const playerChoose = () => {
-	let choices = document.getElementsByClassName("game-choice") 
-	Array.from(choices).forEach(choice=>choice.addEventListener("click", (e)=>{
-		console.log(choice.getAttribute("key-choice"))
-	}))
-}
+let wins = 0;
+let lost = 0;
+let ties = 0;
+let rounds = 1;
+//UI
+const choices = document.getElementsByClassName("game-choice") 
+const scores = document.getElementsByClassName("score")
+const roundCounter = document.querySelector(".round-counter")
 
-const computerChoose = () => {
+
+let computerChoose = () => {
 	let choices = ["rock", "paper", "scissor"]
 	let randNum = Math.floor(Math.random() * 3)
 	return choices[randNum]
 }
 
-const round = (playerChoice, computerChoice) => {
-
+let round = (playerChoice, computerChoice) => {
+	let computerChosen = computerChoice()
+	if(playerChoice == "rock" && computerChosen == "scissor" 
+	|| playerChoice == "scissor" && computerChosen == "paper" 
+	|| playerChoice == "paper" && computerChosen == "rock"){
+		wins += 1
+		rounds += 1
+	}else if(playerChoice == computerChosen){
+		ties += 1
+		rounds += 1
+	}else{
+		lost += 1
+		rounds += 1
+	}
 }
 
-const sayHello = () => {
-	console.log("hello")
+let restartGame = () => {
+	rounds = 1;
+	wins = 0;
+	lost = 0;
+	ties = 0;
 }
+
+let checkGame = () => {
+	if(rounds >	 5){
+		return true
+	}
+}
+
+//game 
+Array.from(choices).forEach(choice=>choice.addEventListener("click", (e)=>{
+	round(choice.getAttribute("key-choice"), computerChoose)
+	scores[0].textContent = wins
+	scores[1].textContent = lost
+	scores[2].textContent = ties
+	roundCounter.textContent = rounds
+	checkGame()
+}))
+
 
